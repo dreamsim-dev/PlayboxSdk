67c1ebe4bc2adeddf6ada440441858f2db2f638b
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-05-27 13:54:15 +0300

    Update PlayboxSplashLogger.cs

--- Runtime/Any/Scripts/Initializations/PlayboxSplashLogger.cs
+++ Runtime/Any/Scripts/Initializations/PlayboxSplashLogger.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine.Events;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace Playbox
 {
     public class PlayboxSplashLogger : PlayboxBehaviour
     {
+        [SerializeField]
+        private UnityAction<string> OnSplash;
+        
         public static UnityAction<string> SplashEvent;
+
+        private void Start()
+        {
+            SplashEvent += (arg0) => OnSplash?.Invoke(arg0);
+        }
     }
 }
