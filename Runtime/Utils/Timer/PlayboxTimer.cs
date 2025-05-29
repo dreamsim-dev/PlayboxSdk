50b6c0b98520f44406a98fbf3ab74a58f2a8aa31
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-05-29 15:24:20 +0300

    update md documentation

--- Runtime/Utils/Timer/PlayboxTimer.cs
+++ Runtime/Utils/Timer/PlayboxTimer.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 
+/*
+ * @namespace Utils.Timer
+ * @brief Timer utils
+ * 
+ */
 namespace Utils.Timer
 {
+    /// <summary>
+    /// Timer with events
+    /// </summary>
     public class PlayboxTimer
     {
+        /// <summary>
+        /// Called when the timer starts
+        /// </summary>
         public event Action OnTimerStart;
+        /// <summary>
+        /// Called when the timer has been forced to stop
+        /// </summary>
         public event Action OnTimerStopped;
+        /// <summary>
+        /// Called when the timer has been paused
+        /// </summary>
         public event Action OnTimerPaused;
+        /// <summary>
+        /// Called when the timer has been unpaused
+        /// </summary>
         public event Action OnTimerResumed;
+        /// <summary>
+        /// Called when the time is up
+        /// </summary>
         public event Action OnTimeOut;
+        /// <summary>
+        /// Refreshes and returns the time elapsed from the beginning
+        /// </summary>
         public event Action<float> OnTimeElapsed;
+        /// <summary>
+        /// Returns the time remaining from the start
+        /// </summary>
         public event Action<float> OnTimeRemaining;
+        /// <summary>
+        /// The field responsible for the initial time
+        /// </summary>
 
         public float initialTime { get; set; } = 5;
         
@@ -19,6 +51,9 @@ namespace Utils.Timer
 
         private bool isPaused;
         
+        /// <summary>
+        /// starts the timer from the beginning
+        /// </summary>
         public void Start()
         {
             timeElapsed = 0;
@@ -27,6 +62,9 @@ namespace Utils.Timer
             OnTimerStart?.Invoke();
         }
         
+        /// <summary>
+        /// Forced stop of the timer
+        /// </summary>
         public void Stop()
         {
             OnTimeElapsed?.Invoke(timeElapsed);
@@ -37,13 +75,17 @@ namespace Utils.Timer
             
             OnTimerStopped?.Invoke();
         }
-
+        /// <summary>
+        /// Restarting the timer
+        /// </summary>
         public void Restart()
         {
             Stop();
             Start();
         }
-
+        /// <summary>
+        /// Setting the timer to pause
+        /// </summary>
         public void Pause()
         {
             isPaused = true;
@@ -52,7 +94,9 @@ namespace Utils.Timer
             OnTimeElapsed?.Invoke(timeElapsed);
             OnTimeRemaining?.Invoke(timeRemaining);
         }
-
+        /// <summary>
+        /// Unpausing the timer
+        /// </summary>
         public void Resume()
         {
             isPaused = false;
@@ -61,7 +105,10 @@ namespace Utils.Timer
             OnTimeElapsed?.Invoke(timeElapsed);
             OnTimeRemaining?.Invoke(timeRemaining);
         }
-        
+        /// <summary>
+        /// Method for updating the timer
+        /// </summary>
+        /// <param name="deltaTime">Delta time from <code>Time.deltaTime</code></param>
         public void Update(float deltaTime)
         {
             if (isPaused)
---
+++ Runtime/Utils/Timer/Timer.md
@@ -0,0 +1,27 @@
+﻿# Playbox timer
+
+
+> The timer is necessary to facilitate interaction with time variables and remove their low-level use.
+> 
+> PlayboxTimer : [PlayboxTimer Documentation](@ref Utils.Timer.PlayboxTimer)
+> 
+> Before you start working with the timer, you need to create an instance of the PlayboxTimer class :
+> 
+>    <code data-lang="csharp">PlayboxTimer timer = new PlayboxTimer();</code>
+> 
+> We can then optionally specify the start time of the timer
+> 
+>    <code data-lang="csharp">timer.initialTime = 5.0f;</code>
+> 
+>The PlayboxTimer class has many [callbacks](@ref Utils.Timer.PlayboxTimer.OnTimerStart) to make it easy to use. 
+> [See here for details](@ref Utils.Timer.PlayboxTimer.OnTimerStart).
+> But now we will use the [OnTimeOut](@ref Utils.Timer.PlayboxTimer.OnTimeOut) event.
+> 
+>    <code data-lang="csharp">timer.OnTimeOut += (){ Debug.Log("TimeOut"); }</code> 
+> 
+> This event will be triggered only when the timer time expires.
+> 
+> But to start the timer, you must call the [timer.Start();](@ref Utils.Timer.PlayboxTimer.Start) method;
+> 
+>    <code data-lang="csharp">timer.Start();</code>
+> 
