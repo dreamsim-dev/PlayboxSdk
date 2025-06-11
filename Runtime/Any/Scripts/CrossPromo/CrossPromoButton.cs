9605b6756396df72a5300b3753d330dd37605723
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-06-11 12:36:02 +0300

    add cross promo sample

---
+++ Runtime/Any/Scripts/CrossPromo/CrossPromoButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Playbox
+{
+    public class CrossPromoButton: MonoBehaviour
+    {
+        [SerializeField]
+        private string promotedID = "world.dreamsim.slumdogbillionaire";
+        
+        [SerializeField]
+        private string campaign = "dreamsim";
+        
+        public void Click()
+        {
+            Dictionary<string,string> properties = new ();
+            
+            properties.Add("campaign", campaign);
+            properties.Add("promoted_id", promotedID);
+            
+            CrossPromo.RecordCrossPromoImpression(promotedID,campaign, properties);
+        }
+    }
+}
